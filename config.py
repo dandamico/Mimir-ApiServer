6cef3aeee015fe0afca766ef6115c9c3fc9470f2
Daniele D'Amico <danieledamico16@gmail.com>
2020-12-03 14:20:17 +0100

    daniele

--- config.py
+++ config.py
@@ -3,25 +3,29 @@ import connexion
 from flask_sqlalchemy import SQLAlchemy
 import mysql.connector as mysql
 
+MYSQL_HOST=os.environ.get("MYSQL_HOST")
+MYSQL_PSSW=os.environ.get("MYSQL_PASSWORD")
+MYSQL_USER=os.environ.get("MYSQL_USER")
+MYSQL_URL=os.environ.get("MYSQL_URL")
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 connex_app = connexion.App(__name__,specification_dir=basedir)
 
 app = connex_app.app
 
-mysql_url = 'mysql+pymysql://root:0satellite0@localhost/mimir'
-
 def createDB():
 	db = mysql.connect(
-		host= "localhost",
-		user = "root",
-		passwd="0satellite0"
+		host=MYSQL_HOST,
+		user=MYSQL_USER,
+		passwd=MYSQL_PSSW
+		
 		)
 	cursor = db.cursor()
 	cursor.execute("CREATE DATABASE mimir")
 
 app.config["SQLALCHEMY_ECHO"] = False
-app.config["SQLALCHEMY_DATABASE_URI"] = mysql_url
+app.config["SQLALCHEMY_DATABASE_URI"] = MYSQL_URL
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
